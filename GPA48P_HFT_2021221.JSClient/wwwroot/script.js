752c5293dd2fd815b8ed0487bcf58e9af818369a
Horvath Matyas <horvath.matyas@stud.uni-obuda.hu>
2022-04-28 15:59:39 +0200

    wwwroot modified

---
+++ GPA48P_HFT_2021221.JSClient/wwwroot/script.js
@@ -0,0 +1,20 @@
+﻿let actors = [];
+
+fetch('http://localhost:62480/animalShelter')
+    .then(x => x.json())
+    .then(y => {
+        shelters = y;
+        console.log(shelters);
+        display();
+    });
+
+function display() {
+    shelters.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.shelterId + "</td><td>"
+            + t.shelterName + "</td><td>"
+            + t.address + "</td><td>"
+            + t.phoneNumber + "</td><td>"
+            + t.taxNumber + "</td></tr";
+    });
+}
